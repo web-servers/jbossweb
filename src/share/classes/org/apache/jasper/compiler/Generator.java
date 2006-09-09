e3cdcf2a1b7ce74310606f89660ac546fde08507
remy.maucherat <rmaucher@users.noreply.github.com>
2006-09-09 21:20:03 +0000

    - Port patch.

--- src/share/classes/org/apache/jasper/compiler/Generator.java
+++ src/share/classes/org/apache/jasper/compiler/Generator.java
@@ -70,8 +70,10 @@ class Generator {
 
     private static final Class[] OBJECT_CLASS = { Object.class };
 
-    private static final String VAR_EXPRESSIONFACTORY = "_el_expressionfactory";
-    private static final String VAR_ANNOTATIONPROCESSOR = "_jsp_annotationprocessor";
+    private static final String VAR_EXPRESSIONFACTORY = 
+        System.getProperty("org.apache.jasper.compiler.Generator.VAR_EXPRESSIONFACTORY", "_el_expressionfactory");
+    private static final String VAR_ANNOTATIONPROCESSOR = 
+        System.getProperty("org.apache.jasper.compiler.Generator.VAR_ANNOTATIONPROCESSOR", "_jsp_annotationprocessor");
 
     private ServletWriter out;
 
@@ -2922,7 +2924,12 @@ class Generator {
             }
 
             // Set parent
-            if (!simpleTag) {
+            if (isTagFile && parent == null) {
+                out.printin(tagHandlerVar);
+                out.print(".setParent(");
+                out.print("new javax.servlet.jsp.tagext.TagAdapter(");
+                out.print("(javax.servlet.jsp.tagext.SimpleTag) this ));");
+            } else if (!simpleTag) {
                 out.printin(tagHandlerVar);
                 out.print(".setParent(");
                 if (parent != null) {
