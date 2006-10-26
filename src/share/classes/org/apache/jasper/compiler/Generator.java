a96383cb76409d101a5279d32da837f2daa55fa8
remy.maucherat <rmaucher@users.noreply.github.com>
2006-10-26 23:15:28 +0000

    - Some deferred fixes.

--- src/share/classes/org/apache/jasper/compiler/Generator.java
+++ src/share/classes/org/apache/jasper/compiler/Generator.java
@@ -1,9 +1,10 @@
 /*
- * Copyright 1999,2004-2006 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -891,9 +892,9 @@ class Generator {
 
         public void visit(Node.ELExpression n) throws JasperException {
             n.setBeginJavaLine(out.getJavaLine());
-            if (!pageInfo.isELIgnored()) {
+            if (!pageInfo.isELIgnored() && (n.getEL() != null)) {
                 out.printil("out.write("
-                        + JspUtil.interpreterCall(this.isTagFile, "${"
+                        + JspUtil.interpreterCall(this.isTagFile, n.getType() + "{"
                                 + new String(n.getText()) + "}", String.class,
                                 n.getEL().getMapName(), false) + ");");
             } else {
