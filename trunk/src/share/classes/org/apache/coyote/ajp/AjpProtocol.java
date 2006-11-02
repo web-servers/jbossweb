4f86a31f598918a6f0e778c921410d9c365114e8
mladen.turk@jboss.com <mladen.turk@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2006-11-02 07:19:07 +0000

    Log the non default AJP packet sizes.

--- trunk/src/share/classes/org/apache/coyote/ajp/AjpProtocol.java
+++ trunk/src/share/classes/org/apache/coyote/ajp/AjpProtocol.java
@@ -195,6 +195,9 @@ public class AjpProtocol
         }
         if (log.isInfoEnabled()) {
             log.info(sm.getString("ajpprotocol.init", getName()));
+            if (packetSize != Constants.MAX_PACKET_SIZE) {
+                log.info(sm.getString("ajpprotocol.packetsize", "" + packetSize));
+            }            
         }
     }
 
