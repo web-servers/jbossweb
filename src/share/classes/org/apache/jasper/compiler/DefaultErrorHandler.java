29c85dcce9f3cc61ac3f2311dcefb4982febd1d4
remy.maucherat <rmaucher@users.noreply.github.com>
2007-03-13 17:12:24 +0000

    - Port Jasper fixes.

--- src/share/classes/org/apache/jasper/compiler/DefaultErrorHandler.java
+++ src/share/classes/org/apache/jasper/compiler/DefaultErrorHandler.java
@@ -67,18 +67,25 @@ class DefaultErrorHandler implements ErrorHandler {
         StringBuffer buf = new StringBuffer();
         
         for (int i=0; i < details.length; i++) {
-            buf.append("\n");
             if (details[i].getJspBeginLineNumber() >= 0) {
                 args = new Object[] {
                         new Integer(details[i].getJspBeginLineNumber()), 
                         details[i].getJspFileName() };
-                buf.append("\n");
+                buf.append("\n\n");
                 buf.append(Localizer.getMessage("jsp.error.single.line.number",
                         args));
                 buf.append("\n");
                 buf.append(details[i].getErrorMessage());
                 buf.append("\n");
                 buf.append(details[i].getJspExtract());
+            } else {
+                args = new Object[] {
+                        new Integer(details[i].getJavaLineNumber()) };
+                buf.append("\n\n");
+                buf.append(Localizer.getMessage("jsp.error.java.line.number",
+                        args));
+                buf.append("\n");
+                buf.append(details[i].getErrorMessage());
             }
         }
         buf.append("\n\nStacktrace:");
