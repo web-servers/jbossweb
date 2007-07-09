c960d1db2dfa09f290e5ec641d701572f124d359
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2007-07-09 22:26:10 +0000

    - Port Tomcat patch.

--- trunk/java/org/apache/catalina/manager/HTMLManagerServlet.java
+++ trunk/java/org/apache/catalina/manager/HTMLManagerServlet.java
@@ -305,7 +305,11 @@ public final class HTMLManagerServlet extends ManagerServlet {
         // Message Section
         args = new Object[3];
         args[0] = sm.getString("htmlManagerServlet.messageLabel");
-        args[1] = (message == null || message.length() == 0) ? "OK" : message;
+        if (message == null || message.length() == 0) {
+            args[1] = "OK";
+        } else {
+            args[1] = RequestUtil.filter(message);
+        }
         writer.print(MessageFormat.format(Constants.MESSAGE_SECTION, args));
 
         // Manager Section
