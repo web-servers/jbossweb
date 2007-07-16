882baab22b0ca377cecef8f49253d01dd21b5e0b
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2007-07-16 10:25:56 +0000

    - Port patch fixing handling of ROOT.xml.

--- trunk/java/org/apache/catalina/startup/ContextConfig.java
+++ trunk/java/org/apache/catalina/startup/ContextConfig.java
@@ -64,7 +64,7 @@ import org.xml.sax.SAXParseException;
  *
  * @author Craig R. McClanahan
  * @author Jean-Francois Arcand
- * @version $Revision: 500616 $ $Date: 2007-01-27 22:00:07 +0100 (sam., 27 janv. 2007) $
+ * @version $Revision: 556489 $ $Date: 2007-07-16 04:01:01 +0200 (lun., 16 juil. 2007) $
  */
 
 public class ContextConfig
@@ -881,12 +881,12 @@ public class ContextConfig
         file = new File(docBase);
         String origDocBase = docBase;
         
+        String contextPath = context.getPath();
+        if (contextPath.equals("")) {
+            contextPath = "ROOT";
+        }
         if (docBase.toLowerCase().endsWith(".war") && !file.isDirectory() && unpackWARs) {
             URL war = new URL("jar:" + (new File(docBase)).toURL() + "!/");
-            String contextPath = context.getPath();
-            if (contextPath.equals("")) {
-                contextPath = "ROOT";
-            }
             docBase = ExpandWar.expand(host, war, contextPath);
             file = new File(docBase);
             docBase = file.getCanonicalPath();
@@ -900,7 +900,7 @@ public class ContextConfig
                 if (warFile.exists()) {
                     if (unpackWARs) {
                         URL war = new URL("jar:" + warFile.toURL() + "!/");
-                        docBase = ExpandWar.expand(host, war, context.getPath());
+                        docBase = ExpandWar.expand(host, war, contextPath);
                         file = new File(docBase);
                         docBase = file.getCanonicalPath();
                     } else {
