e198e23c07a9170111537959ec05326b558e2a96
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2008-03-27 17:49:20 +0000

    - Move to MultiMap backend (I never reviewed this class and did not realize it was still based on a Hashtable). - The old class stays there just in case for now as Parameters2.

--- trunk/java/org/apache/tomcat/util/http/Parameters2.java
+++ trunk/java/org/apache/tomcat/util/http/Parameters2.java
@@ -18,8 +18,8 @@
 package org.apache.tomcat.util.http;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.Hashtable;
 
 import org.apache.tomcat.util.buf.ByteChunk;
 import org.apache.tomcat.util.buf.CharChunk;
@@ -30,60 +30,131 @@ import org.apache.tomcat.util.collections.MultiMap;
 /**
  * 
  * @author Costin Manolache
- * @author Remy Maucherat
+ * @deprecated
  */
-public final class Parameters extends MultiMap {
+public final class Parameters2 extends MultiMap {
 
-    protected static org.jboss.logging.Logger log =
-        org.jboss.logging.Logger.getLogger(Parameters.class);
-
-    public static final int INITIAL_SIZE = 8;
-    protected static final String[] ARRAY_TYPE = new String[0];
-
-    protected boolean didQueryParameters = false;
-    protected boolean didMerge = false;
     
-    protected MessageBytes queryMB;
+    private static org.jboss.logging.Logger log=
+        org.jboss.logging.Logger.getLogger(Parameters2.class );
+    
+    // Transition: we'll use the same Hashtable( String->String[] )
+    // for the beginning. When we are sure all accesses happen through
+    // this class - we can switch to MultiMap
+    private Hashtable paramHashStringArray=new Hashtable();
+    private boolean didQueryParameters=false;
+    private boolean didMerge=false;
+    
+    MessageBytes queryMB;
+    MimeHeaders  headers;
 
-    protected UDecoder urlDec;
-    protected MessageBytes decodedQuery = MessageBytes.newInstance();
+    UDecoder urlDec;
+    MessageBytes decodedQuery=MessageBytes.newInstance();
+    
+    public static final int INITIAL_SIZE=4;
 
-    protected String encoding = null;
-    protected String queryStringEncoding = null;
+    // Garbage-less parameter merging.
+    // In a sub-request with parameters, the new parameters
+    // will be stored in child. When a getParameter happens,
+    // the 2 are merged togheter. The child will be altered
+    // to contain the merged values - the parent is allways the
+    // original request.
+    private Parameters2 child=null;
+    private Parameters2 parent=null;
+    private Parameters2 currentChild=null;
+
+    String encoding=null;
+    String queryStringEncoding=null;
     
     /**
      * 
      */
-    public Parameters() {
-        super(INITIAL_SIZE);
+    public Parameters2() {
+        super( INITIAL_SIZE );
     }
 
-    public void setQuery(MessageBytes queryMB) {
-        this.queryMB = queryMB;
+    public void setQuery( MessageBytes queryMB ) {
+        this.queryMB=queryMB;
     }
 
-    public void setHeaders(MimeHeaders headers) {
-        // Not used anymore at the moment
+    public void setHeaders( MimeHeaders headers ) {
+        this.headers=headers;
     }
 
-    public void setEncoding(String s) {
-        encoding = s;
+    public void setEncoding( String s ) {
+        encoding=s;
+        if(debug>0) log( "Set encoding to " + s );
     }
 
-    public void setURLDecoder(UDecoder u) {
-        urlDec = u;
-    }
-
-    public void setQueryStringEncoding(String s) {
-        queryStringEncoding = s;
+    public void setQueryStringEncoding( String s ) {
+        queryStringEncoding=s;
+        if(debug>0) log( "Set query string encoding to " + s );
     }
 
     public void recycle() {
         super.recycle();
-        didQueryParameters = false;
-        didMerge = false;
-        encoding = null;
+        paramHashStringArray.clear();
+        didQueryParameters=false;
+        currentChild=null;
+        didMerge=false;
+        encoding=null;
         decodedQuery.recycle();
+    }
+    
+    // -------------------- Sub-request support --------------------
+
+    public Parameters2 getCurrentSet() {
+        if( currentChild==null )
+            return this;
+        return currentChild;
+    }
+    
+    /** Create ( or reuse ) a child that will be used during a sub-request.
+        All future changes ( setting query string, adding parameters )
+        will affect the child ( the parent request is never changed ).
+        Both setters and getters will return the data from the deepest
+        child, merged with data from parents.
+    */
+    public void push() {
+        // We maintain a linked list, that will grow to the size of the
+        // longest include chain.
+        // The list has 2 points of interest:
+        // - request.parameters() is the original request and head,
+        // - request.parameters().currentChild() is the current set.
+        // The ->child and parent<- links are preserved ( currentChild is not
+        // the last in the list )
+        
+        // create a new element in the linked list
+        // note that we reuse the child, if any - pop will not
+        // set child to null !
+        if( currentChild==null ) {
+            currentChild=new Parameters2();
+            currentChild.setURLDecoder( urlDec );
+            currentChild.parent=this;
+            return;
+        }
+        if( currentChild.child==null ) {
+            currentChild.child=new Parameters2();
+            currentChild.setURLDecoder( urlDec );
+            currentChild.child.parent=currentChild;
+        } // it is not null if this object already had a child
+        // i.e. a deeper include() ( we keep it )
+
+        // the head will be the new element.
+        currentChild=currentChild.child;
+        currentChild.setEncoding( encoding );
+    }
+
+    /** Discard the last child. This happens when we return from a
+        sub-request and the parameters are locally modified.
+     */
+    public void pop() {
+        if( currentChild==null ) {
+            throw new RuntimeException( "Attempt to pop without a push" );
+        }
+        currentChild.recycle();
+        currentChild=currentChild.parent;
+        // don't remove the top.
     }
     
     // -------------------- Data access --------------------
@@ -92,44 +163,83 @@ public final class Parameters extends MultiMap {
     
     // This is the original data representation ( hash of String->String[])
 
-    public void addParameterValues(String name, String[] values) {
-        if (name == null || values == null) {
-            return;
+    public void addParameterValues( String key, String[] newValues) {
+        if ( key==null ) return;
+        String values[];
+        if (paramHashStringArray.containsKey(key)) {
+            String oldValues[] = (String[])paramHashStringArray.get(key);
+            values = new String[oldValues.length + newValues.length];
+            for (int i = 0; i < oldValues.length; i++) {
+                values[i] = oldValues[i];
+            }
+            for (int i = 0; i < newValues.length; i++) {
+                values[i+ oldValues.length] = newValues[i];
+            }
+        } else {
+            values = newValues;
         }
-        for (int i = 0; i < values.length; i++) {
-            String value = values[i];
-            int pos = addField();
-            getName(pos).setString(name);
-            getValue(pos).setString(value);
-        }
+
+        paramHashStringArray.put(key, values);
     }
 
     public String[] getParameterValues(String name) {
         handleQueryParameters();
-        int pos = findFirst(name);
-        if (pos > 0) {
-            ArrayList<String> result = new ArrayList<String>();
-            while (pos > 0) {
-                result.add(getValue(pos).toString());
-                pos = findNext(pos);
-            }
-            return result.toArray(ARRAY_TYPE);
-        } else {
-            return null;
+        // sub-request
+        if( currentChild!=null ) {
+            currentChild.merge();
+            return (String[])currentChild.paramHashStringArray.get(name);
         }
+
+        // no "facade"
+        String values[]=(String[])paramHashStringArray.get(name);
+        return values;
     }
  
     public Enumeration getParameterNames() {
         handleQueryParameters();
-        return names(); 
+        // Slow - the original code
+        if( currentChild!=null ) {
+            currentChild.merge();
+            return currentChild.paramHashStringArray.keys();
+        }
+
+        // merge in child
+        return paramHashStringArray.keys();
     }
 
-    // Shortcut.
-    public String getParameter(String name) {
+    /** Combine the parameters from parent with our local ones
+     */
+    private void merge() {
+        // recursive
+        if( debug > 0 ) {
+            log("Before merging " + this + " " + parent + " " + didMerge );
+            log(  paramsAsString());
+        }
+        // Local parameters first - they take precedence as in spec.
         handleQueryParameters();
-        int pos = findFirst(name);
-        if (pos > 0) {
-            return getValue(pos).toString();
+
+        // we already merged with the parent
+        if( didMerge ) return;
+
+        // we are the top level
+        if( parent==null ) return;
+
+        // Add the parent props to the child ( lower precedence )
+        parent.merge();
+        Hashtable parentProps=parent.paramHashStringArray;
+        merge2( paramHashStringArray , parentProps);
+        didMerge=true;
+        if(debug > 0 )
+            log("After " + paramsAsString());
+    }
+
+
+    // Shortcut.
+    public String getParameter(String name ) {
+        String[] values = getParameterValues(name);
+        if (values != null) {
+            if( values.length==0 ) return "";
+            return values[0];
         } else {
             return null;
         }
@@ -157,24 +267,80 @@ public final class Parameters extends MultiMap {
         processParameters( decodedQuery, queryStringEncoding );
     }
 
-    protected void addParam( String name, String value ) {
-        if (name == null) {
-            return;
+    // --------------------
+    
+    /** Combine 2 hashtables into a new one.
+     *  ( two will be added to one ).
+     *  Used to combine child parameters ( RequestDispatcher's query )
+     *  with parent parameters ( original query or parent dispatcher )
+     */
+    private static void merge2(Hashtable one, Hashtable two ) {
+        Enumeration e = two.keys();
+
+        while (e.hasMoreElements()) {
+            String name = (String) e.nextElement();
+            String[] oneValue = (String[]) one.get(name);
+            String[] twoValue = (String[]) two.get(name);
+            String[] combinedValue;
+
+            if (twoValue == null) {
+                continue;
+            } else {
+                if( oneValue==null ) {
+                    combinedValue = new String[twoValue.length];
+                    System.arraycopy(twoValue, 0, combinedValue,
+                                     0, twoValue.length);
+                } else {
+                    combinedValue = new String[oneValue.length +
+                                               twoValue.length];
+                    System.arraycopy(oneValue, 0, combinedValue, 0,
+                                     oneValue.length);
+                    System.arraycopy(twoValue, 0, combinedValue,
+                                     oneValue.length, twoValue.length);
+                }
+                one.put(name, combinedValue);
+            }
         }
-        int pos = addField();
-        getName(pos).setString(name);
-        getValue(pos).setString(value);
+    }
+
+    // incredibly inefficient data representation for parameters,
+    // until we test the new one
+    private void addParam( String key, String value ) {
+        if( key==null ) return;
+        String values[];
+        if (paramHashStringArray.containsKey(key)) {
+            String oldValues[] = (String[])paramHashStringArray.
+                get(key);
+            values = new String[oldValues.length + 1];
+            for (int i = 0; i < oldValues.length; i++) {
+                values[i] = oldValues[i];
+            }
+            values[oldValues.length] = value;
+        } else {
+            values = new String[1];
+            values[0] = value;
+        }
+        
+        
+        paramHashStringArray.put(key, values);
+    }
+
+    public void setURLDecoder( UDecoder u ) {
+        urlDec=u;
     }
 
     // -------------------- Parameter parsing --------------------
 
+    // This code is not used right now - it's the optimized version
+    // of the above.
+
     // we are called from a single thread - we can do it the hard way
     // if needed
-    protected ByteChunk tmpName = new ByteChunk();
-    protected ByteChunk tmpValue = new ByteChunk();
-    protected CharChunk tmpNameC = new CharChunk(32);
-    protected CharChunk tmpValueC = new CharChunk(128);
-
+    ByteChunk tmpName=new ByteChunk();
+    ByteChunk tmpValue=new ByteChunk();
+    CharChunk tmpNameC=new CharChunk(1024);
+    CharChunk tmpValueC=new CharChunk(1024);
+    
     public void processParameters( byte bytes[], int start, int len ) {
         processParameters(bytes, start, len, encoding);
     }
@@ -350,11 +516,11 @@ public final class Parameters extends MultiMap {
      */
     public String paramsAsString() {
         StringBuffer sb=new StringBuffer();
-        Enumeration en= names();
+        Enumeration en= paramHashStringArray.keys();
         while( en.hasMoreElements() ) {
             String k=(String)en.nextElement();
             sb.append( k ).append("=");
-            String v[]=(String[])getParameterValues(k);
+            String v[]=(String[])paramHashStringArray.get( k );
             for( int i=0; i<v.length; i++ )
                 sb.append( v[i] ).append(",");
             sb.append("\n");
