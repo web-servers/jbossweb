1e27b490c46ecb1fe5e88ab52bc7f52bcaf3fa64
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2008-03-31 10:02:17 +0000

    - Remove the collections, which are useless old code (if needed, they are easy to find anyway). - As it is a specialized structure like MimeHeaders, repackage all relevant MultiMap code in Parameters   (this avoids exposing broken code like remove). - Repackage all package private classes as protected inner classes.

--- trunk/java/org/apache/tomcat/util/http/Parameters.java
+++ trunk/java/org/apache/tomcat/util/http/Parameters.java
@@ -25,24 +25,114 @@ import org.apache.tomcat.util.buf.ByteChunk;
 import org.apache.tomcat.util.buf.CharChunk;
 import org.apache.tomcat.util.buf.MessageBytes;
 import org.apache.tomcat.util.buf.UDecoder;
-import org.apache.tomcat.util.collections.MultiMap;
 
 /**
  * 
  * @author Costin Manolache
  * @author Remy Maucherat
  */
-public final class Parameters extends MultiMap {
+public final class Parameters {
 
-    protected static org.jboss.logging.Logger log =
-        org.jboss.logging.Logger.getLogger(Parameters.class);
+    protected static org.jboss.logging.Logger log = org.jboss.logging.Logger
+            .getLogger(Parameters.class);
 
+    protected static final int NEED_NEXT = -2;
+    protected static final int LAST = -1;
     public static final int INITIAL_SIZE = 8;
     protected static final String[] ARRAY_TYPE = new String[0];
 
+    protected class Field {
+        MessageBytes name = MessageBytes.newInstance();
+        MessageBytes value = MessageBytes.newInstance();
+
+        // Extra info for speed
+
+        // multiple fields with same name - a linked list will
+        // speed up multiple name enumerations and search.
+        int nextPos;
+
+        // hashkey
+        int hash;
+        Field nextSameHash;
+
+        Field() {
+            nextPos = NEED_NEXT;
+        }
+
+        void recycle() {
+            name.recycle();
+            value.recycle();
+            nextPos = NEED_NEXT;
+        }
+    }
+
+    /**
+     * Enumerate the distinct header names. Each nextElement() is O(n) ( a
+     * comparation is done with all previous elements ).
+     * 
+     * This is less frequesnt than add() - we want to keep add O(1).
+     */
+    protected class NamesEnumeration implements Enumeration {
+        int pos;
+        String next;
+
+        // toString and unique options are not implemented -
+        // we allways to toString and unique.
+
+        /**
+         * Create a new multi-map enumeration.
+         * 
+         * @param headers
+         *            the collection to enumerate
+         * @param toString
+         *            convert each name to string
+         * @param unique
+         *            return only unique names
+         */
+        public NamesEnumeration() {
+            pos = 0;
+            findNext();
+        }
+
+        private void findNext() {
+            next = null;
+            for (; pos < count; pos++) {
+                next = getName(pos).toString();
+                for (int j = 0; j < pos; j++) {
+                    if (getName(j).equalsIgnoreCase(next)) {
+                        // duplicate.
+                        next = null;
+                        break;
+                    }
+                }
+                if (next != null) {
+                    // it's not a duplicate
+                    break;
+                }
+            }
+            // next time findNext is called it will try the
+            // next element
+            pos++;
+        }
+
+        public boolean hasMoreElements() {
+            return next != null;
+        }
+
+        public Object nextElement() {
+            String current = next;
+            findNext();
+            return current;
+        }
+    }
+
+    protected Field[] fields;
+    // fields in use
+    protected int count;
+
     protected boolean didQueryParameters = false;
     protected boolean didMerge = false;
-    
+
     protected MessageBytes queryMB;
 
     protected UDecoder urlDec;
@@ -50,12 +140,12 @@ public final class Parameters extends MultiMap {
 
     protected String encoding = null;
     protected String queryStringEncoding = null;
-    
+
     /**
      * 
      */
     public Parameters() {
-        super(INITIAL_SIZE);
+        fields = new Field[INITIAL_SIZE];
     }
 
     public void setQuery(MessageBytes queryMB) {
@@ -79,17 +169,93 @@ public final class Parameters extends MultiMap {
     }
 
     public void recycle() {
-        super.recycle();
+        for (int i = 0; i < count; i++) {
+            fields[i].recycle();
+        }
+        count = 0;
         didQueryParameters = false;
         didMerge = false;
         encoding = null;
         decodedQuery.recycle();
     }
-    
+
+    /**
+     * Returns the current number of header fields.
+     */
+    protected int size() {
+        return count;
+    }
+
+    /**
+     * Returns the Nth header name This may be used to iterate through all
+     * header fields.
+     * 
+     * An exception is thrown if the index is not valid ( <0 or >size )
+     */
+    protected MessageBytes getName(int n) {
+        // n >= 0 && n < count ? headers[n].getName() : null
+        return fields[n].name;
+    }
+
+    /**
+     * Returns the Nth header value This may be used to iterate through all
+     * header fields.
+     */
+    protected MessageBytes getValue(int n) {
+        return fields[n].value;
+    }
+
+    /**
+     * Create a new, unitialized entry.
+     */
+    protected int addField() {
+        int len = fields.length;
+        int pos = count;
+        if (count >= len) {
+            // expand header list array
+            Field tmp[] = new Field[pos * 2];
+            System.arraycopy(fields, 0, tmp, 0, len);
+            fields = tmp;
+        }
+        if (fields[pos] == null) {
+            fields[pos] = new Field();
+        }
+        count++;
+        return pos;
+    }
+
+    protected int findFirst(String name) {
+        for (int i = 0; i < count; i++) {
+            if (fields[i].name.equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    protected int findNext(int startPos) {
+        int next = fields[startPos].nextPos;
+        if (next != NEED_NEXT) {
+            return next;
+        }
+
+        // next==NEED_NEXT, we never searched for this header
+        MessageBytes name = fields[startPos].name;
+        for (int i = (startPos + 1); i < count; i++) {
+            if (fields[i].name.equals(name)) {
+                // cache the search result
+                fields[startPos].nextPos = i;
+                return i;
+            }
+        }
+        fields[startPos].nextPos = LAST;
+        return -1;
+    }
+
     // -------------------- Data access --------------------
     // Access to the current name/values, no side effect ( processing ).
     // You must explicitely call handleQueryParameters and the post methods.
-    
+
     // This is the original data representation ( hash of String->String[])
 
     public void addParameterValues(String name, String[] values) {
@@ -118,10 +284,10 @@ public final class Parameters extends MultiMap {
             return null;
         }
     }
- 
+
     public Enumeration getParameterNames() {
         handleQueryParameters();
-        return names(); 
+        return new NamesEnumeration();
     }
 
     // Shortcut.
@@ -134,30 +300,32 @@ public final class Parameters extends MultiMap {
             return null;
         }
     }
+
     // -------------------- Processing --------------------
     /** Process the query string into parameters
      */
     public void handleQueryParameters() {
-        if( didQueryParameters ) return;
-
-        didQueryParameters=true;
-
-        if( queryMB==null || queryMB.isNull() )
+        if (didQueryParameters)
             return;
-        
-        if( debug > 0  )
-            log( "Decoding query " + decodedQuery + " " + queryStringEncoding);
+
+        didQueryParameters = true;
+
+        if (queryMB == null || queryMB.isNull())
+            return;
+
+        if (debug > 0)
+            log("Decoding query " + decodedQuery + " " + queryStringEncoding);
 
         try {
-            decodedQuery.duplicate( queryMB );
+            decodedQuery.duplicate(queryMB);
         } catch (IOException e) {
             // Can't happen, as decodedQuery can't overflow
             e.printStackTrace();
         }
-        processParameters( decodedQuery, queryStringEncoding );
+        processParameters(decodedQuery, queryStringEncoding);
     }
 
-    protected void addParam( String name, String value ) {
+    protected void addParam(String name, String value) {
         if (name == null) {
             return;
         }
@@ -175,87 +343,88 @@ public final class Parameters extends MultiMap {
     protected CharChunk tmpNameC = new CharChunk(32);
     protected CharChunk tmpValueC = new CharChunk(128);
 
-    public void processParameters( MessageBytes data ) {
+    public void processParameters(MessageBytes data) {
         processParameters(data, encoding);
     }
 
-    public void processParameters( MessageBytes data, String encoding ) {
-        if( data==null || data.isNull() || data.getLength() <= 0 ) return;
+    public void processParameters(MessageBytes data, String encoding) {
+        if (data == null || data.isNull() || data.getLength() <= 0)
+            return;
 
         if (data.getType() != MessageBytes.T_BYTES) {
             data.toBytes();
         }
-        ByteChunk bc=data.getByteChunk();
-        processParameters( bc.getBytes(), bc.getOffset(),
-                           bc.getLength(), encoding);
+        ByteChunk bc = data.getByteChunk();
+        processParameters(bc.getBytes(), bc.getOffset(), bc.getLength(),
+                encoding);
     }
 
-    public void processParameters( byte bytes[], int start, int len ) {
+    public void processParameters(byte bytes[], int start, int len) {
         processParameters(bytes, start, len, encoding);
     }
 
-    public void processParameters( byte bytes[], int start, int len, 
-                                   String enc ) {
-        int end=start+len;
-        int pos=start;
-        
-        if( debug>0 ) 
-            log( "Bytes: " + new String( bytes, start, len ));
+    public void processParameters(byte bytes[], int start, int len, String enc) {
+        int end = start + len;
+        int pos = start;
+
+        if (debug > 0)
+            log("Bytes: " + new String(bytes, start, len));
 
         do {
-            boolean noEq=false;
-            int valStart=-1;
-            int valEnd=-1;
-            
-            int nameStart=pos;
-            int nameEnd=ByteChunk.indexOf(bytes, nameStart, end, '=' );
-            // Workaround for a&b&c encoding
-            int nameEnd2=ByteChunk.indexOf(bytes, nameStart, end, '&' );
-            if( (nameEnd2!=-1 ) &&
-                ( nameEnd==-1 || nameEnd > nameEnd2) ) {
-                nameEnd=nameEnd2;
-                noEq=true;
-                valStart=nameEnd;
-                valEnd=nameEnd;
-                if( debug>0) log("no equal " + nameStart + " " + nameEnd + " " + new String(bytes, nameStart, nameEnd-nameStart) );
-            }
-            if( nameEnd== -1 ) 
-                nameEnd=end;
+            boolean noEq = false;
+            int valStart = -1;
+            int valEnd = -1;
 
-            if( ! noEq ) {
-                valStart= (nameEnd < end) ? nameEnd+1 : end;
-                valEnd=ByteChunk.indexOf(bytes, valStart, end, '&');
-                if( valEnd== -1 ) valEnd = (valStart < end) ? end : valStart;
+            int nameStart = pos;
+            int nameEnd = ByteChunk.indexOf(bytes, nameStart, end, '=');
+            // Workaround for a&b&c encoding
+            int nameEnd2 = ByteChunk.indexOf(bytes, nameStart, end, '&');
+            if ((nameEnd2 != -1) && (nameEnd == -1 || nameEnd > nameEnd2)) {
+                nameEnd = nameEnd2;
+                noEq = true;
+                valStart = nameEnd;
+                valEnd = nameEnd;
+                if (debug > 0)
+                    log("no equal " + nameStart + " " + nameEnd + " "
+                            + new String(bytes, nameStart, nameEnd - nameStart));
             }
-            
-            pos=valEnd+1;
-            
-            if( nameEnd<=nameStart ) {
+            if (nameEnd == -1)
+                nameEnd = end;
+
+            if (!noEq) {
+                valStart = (nameEnd < end) ? nameEnd + 1 : end;
+                valEnd = ByteChunk.indexOf(bytes, valStart, end, '&');
+                if (valEnd == -1)
+                    valEnd = (valStart < end) ? end : valStart;
+            }
+
+            pos = valEnd + 1;
+
+            if (nameEnd <= nameStart) {
                 log.warn("Parameters: Invalid chunk ignored.");
                 continue;
                 // invalid chunk - it's better to ignore
             }
-            tmpName.setBytes( bytes, nameStart, nameEnd-nameStart );
-            tmpValue.setBytes( bytes, valStart, valEnd-valStart );
+            tmpName.setBytes(bytes, nameStart, nameEnd - nameStart);
+            tmpValue.setBytes(bytes, valStart, valEnd - valStart);
 
             try {
-                addParam( urlDecode(tmpName, enc), urlDecode(tmpValue, enc) );
+                addParam(urlDecode(tmpName, enc), urlDecode(tmpValue, enc));
             } catch (IOException e) {
                 // Exception during character decoding: skip parameter
-                log.warn("Parameters: Character decoding failed. " + 
-                         "Parameter skipped.", e);
+                log.warn("Parameters: Character decoding failed. "
+                        + "Parameter skipped.", e);
             }
 
             tmpName.recycle();
             tmpValue.recycle();
 
-        } while( pos<end );
+        } while (pos < end);
     }
 
-    protected String urlDecode(ByteChunk bc, String enc)
-        throws IOException {
-        if( urlDec==null ) {
-            urlDec=new UDecoder();   
+    protected String urlDecode(ByteChunk bc, String enc) throws IOException {
+        if (urlDec == null) {
+            urlDec = new UDecoder();
         }
         urlDec.convert(bc);
         String result = null;
@@ -283,23 +452,24 @@ public final class Parameters extends MultiMap {
     /** Debug purpose
      */
     public String paramsAsString() {
-        StringBuffer sb=new StringBuffer();
-        Enumeration en= names();
-        while( en.hasMoreElements() ) {
-            String k=(String)en.nextElement();
-            sb.append( k ).append("=");
-            String v[]=(String[])getParameterValues(k);
-            for( int i=0; i<v.length; i++ )
-                sb.append( v[i] ).append(",");
+        StringBuffer sb = new StringBuffer();
+        Enumeration en = getParameterNames();
+        while (en.hasMoreElements()) {
+            String k = (String) en.nextElement();
+            sb.append(k).append("=");
+            String v[] = (String[]) getParameterValues(k);
+            for (int i = 0; i < v.length; i++)
+                sb.append(v[i]).append(",");
             sb.append("\n");
         }
         return sb.toString();
     }
 
-    private static int debug=0;
-    private void log(String s ) {
+    private static int debug = 0;
+
+    private void log(String s) {
         if (log.isDebugEnabled())
-            log.debug("Parameters: " + s );
+            log.debug("Parameters: " + s);
     }
-   
+
 }
