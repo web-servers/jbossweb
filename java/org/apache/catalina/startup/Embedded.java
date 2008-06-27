3eacd99a539b2e201cf98169e96694b3fb59b34c
Jean-Frederic Clere <jfclere@gmail.com>
2008-06-27 08:46:12 +0000

    Use better default value.

--- java/org/apache/catalina/startup/Embedded.java
+++ java/org/apache/catalina/startup/Embedded.java
@@ -417,7 +417,7 @@ public class Embedded  extends StandardService implements Lifecycle {
         try {
 
             if (protocol.equals("ajp")) {
-                connector = new Connector("org.apache.jk.server.JkCoyoteHandler");
+                connector = new Connector("org.apache.coyote.ajp.AjpProtocol");
             } else if (protocol.equals("memory")) {
                 connector = new Connector("org.apache.coyote.memory.MemoryProtocolHandler");
             } else if (protocol.equals("http")) {
