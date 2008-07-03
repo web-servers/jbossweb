b0bc3b07f007883a14bae8856a33ff8193f2e523
remy.maucherat <rmaucher@users.noreply.github.com>
2008-07-03 14:14:44 +0000

    - Port latest patch to cleanup possible problems when a reload is going on.

--- java/org/apache/catalina/core/StandardWrapper.java
+++ java/org/apache/catalina/core/StandardWrapper.java
@@ -763,6 +763,8 @@ public class StandardWrapper
             throw new ServletException
               (sm.getString("standardWrapper.unloading", getName()));
 
+        boolean newInstance = false;
+
         // If not SingleThreadedModel, return the same instance every time
         if (!singleThreadModel) {
 
@@ -775,6 +777,12 @@ public class StandardWrapper
                                 log.debug("Allocating non-STM instance");
 
                             instance = loadServlet();
+                            // For non-STM, increment here to prevent a race
+                            // condition with unload. Bug 43683, test case #3
+                            if (!singleThreadModel) {
+                                newInstance = true;
+                                countAllocated++;
+                            }
                         } catch (ServletException e) {
                             throw e;
                         } catch (Throwable e) {
@@ -788,6 +796,11 @@ public class StandardWrapper
             if (!singleThreadModel) {
                 if (log.isTraceEnabled())
                     log.trace("  Returning non-STM instance");
+                // For new instances, count will have been incremented at the
+                // time of creation
+                if (!newInstance) {
+                    countAllocated++;
+                }
                 return (instance);
             }
 
