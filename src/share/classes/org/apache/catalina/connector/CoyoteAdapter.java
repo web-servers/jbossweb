c9fbf85e9fc5d997c372af1e3baa6df6b7fc60d0
Jean-Frederic Clere <jfclere@gmail.com>
2008-07-25 16:29:30 +0000

    Add checkNormalize method to verify that the URI is still normalized after character decoding.

--- src/share/classes/org/apache/catalina/connector/CoyoteAdapter.java
+++ src/share/classes/org/apache/catalina/connector/CoyoteAdapter.java
@@ -351,6 +351,12 @@ public class CoyoteAdapter
             }
             // Character decoding
             convertURI(decodedURI, request);
+            // Check that the URI is still normalized
+            if (!checkNormalize(req.decodedURI())) {
+                res.setStatus(400);
+                res.setMessage("Invalid URI character encoding");
+                return false;
+            }
         } else {
             // The URL is chars or String, and has been sent using an in-memory
             // protocol handler, we have to assume the URL has been properly
@@ -709,6 +715,67 @@ public class CoyoteAdapter
         }
 
         uriBC.setBytes(b, start, end);
+
+        return true;
+
+    }
+
+
+    /**
+     * Check that the URI is normalized following character decoding.
+     * <p>
+     * This method checks for "\", "//", "/./" and "/../". This method will
+     * return false if sequences that are supposed to be normalized still 
+     * present in the URI.
+     * 
+     * @param uriMB URI to be normalized
+     */
+    public static boolean checkNormalize(MessageBytes uriMB) {
+
+        CharChunk uriCC = uriMB.getCharChunk();
+        char[] c = uriCC.getChars();
+        int start = uriCC.getStart();
+        int end = uriCC.getEnd();
+
+        int pos = 0;
+
+        // Check for '\' and for null byte
+        for (pos = start; pos < end; pos++) {
+            if (c[pos] == '\\') {
+                return false;
+            }
+            if (c[pos] == 0) {
+                return false;
+            }
+        }
+
+        // Check for "//"
+        for (pos = start; pos < (end - 1); pos++) {
+            if (c[pos] == '/') {
+                if (c[pos + 1] == '/') {
+                    return false;
+                }
+            }
+        }
+
+        // Check for URI ending with "/." or "/.."
+        if (((end - start) >= 2) && (c[end - 1] == '.')) {
+            if ((c[end - 2] == '/') 
+                    || ((c[end - 2] == '.') 
+                    && (c[end - 3] == '/'))) {
+                return false;
+            }
+        }
+
+        // Check for "/./"
+        if (uriCC.indexOf("/./", 0, 3, 0) >= 0) {
+            return false;
+        }
+
+        // Check for "/./"
+        if (uriCC.indexOf("/../", 0, 4, 0) >= 0) {
+            return false;
+        }
 
         return true;
 
