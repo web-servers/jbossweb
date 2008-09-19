4c260cf6e4b53dab07759e5142c7f9e9a8a6aa2f
remy.maucherat <rmaucher@users.noreply.github.com>
2008-09-19 15:22:38 +0000

    - 45451: Rework escaping of EL.

--- java/org/apache/jasper/compiler/Validator.java
+++ java/org/apache/jasper/compiler/Validator.java
@@ -1337,7 +1337,6 @@ class Validator {
                         }
 
                     } else {
-                        value = value.replace(Constants.ESC, '$');
                         result = new Node.JspAttribute(tai, qName, uri,
                                 localName, value, false, null, dynamic);
                     }
@@ -1692,16 +1691,14 @@ class Validator {
         }
     }
 
-    public static void validate(Compiler compiler, Node.Nodes page)
-            throws JasperException {
-
-        /*
-         * Visit the page/tag directives first, as they are global to the page
-         * and are position independent.
-         */
+    public static void validateDirectives(Compiler compiler, Node.Nodes page)
+    throws JasperException {
         page.visit(new DirectiveVisitor(compiler));
+    }
 
-        // Determine the default output content type
+    public static void validateExDirectives(Compiler compiler, Node.Nodes page)
+    throws JasperException {
+//      Determine the default output content type
         PageInfo pageInfo = compiler.getPageInfo();
         String contentType = pageInfo.getContentType();
 
