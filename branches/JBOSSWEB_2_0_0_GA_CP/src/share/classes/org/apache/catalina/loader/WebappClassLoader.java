a2e18dace18e210185f0bdab57b304bc9cb092f8
jfrederic.clere@jboss.com <jfrederic.clere@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2009-01-09 11:58:24 +0000

    Hack for JBPAPP-869.

--- branches/JBOSSWEB_2_0_0_GA_CP/src/share/classes/org/apache/catalina/loader/WebappClassLoader.java
+++ branches/JBOSSWEB_2_0_0_GA_CP/src/share/classes/org/apache/catalina/loader/WebappClassLoader.java
@@ -111,6 +111,10 @@ public class WebappClassLoader
 
     public static final boolean ENABLE_CLEAR_REFERENCES = 
         Boolean.valueOf(System.getProperty("org.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES", "true")).booleanValue();
+
+    public static final boolean SYSTEM_CL_DELEGATION =
+       Boolean.valueOf(System.getProperty("org.apache.catalina.loader.WebappClassLoader.SYSTEM_CL_DELEGATION", "true")).booleanValue();
+
     
     protected class PrivilegedFindResource
         implements PrivilegedAction {
@@ -1269,15 +1273,17 @@ public class WebappClassLoader
 
         // (0.2) Try loading the class with the system class loader, to prevent
         //       the webapp from overriding J2SE classes
-        try {
-            clazz = system.loadClass(name);
-            if (clazz != null) {
-                if (resolve)
-                    resolveClass(clazz);
-                return (clazz);
+        if (SYSTEM_CL_DELEGATION) {
+            try {
+                clazz = system.loadClass(name);
+                if (clazz != null) {
+                    if (resolve)
+                        resolveClass(clazz);
+                    return (clazz);
+                }
+            } catch (ClassNotFoundException e) {
+                // Ignore
             }
-        } catch (ClassNotFoundException e) {
-            // Ignore
         }
 
         // (0.5) Permission to access this class when using a SecurityManager
