16a206d7665f2397fa18e373c9520b2f7ddaa8bf
remy.maucherat <rmaucher@users.noreply.github.com>
2009-02-23 13:56:06 +0000

    - Port Tomcat patches, incl EL resolver improvement for the security manager.

--- java/org/apache/jasper/el/ExpressionEvaluatorImpl.java
+++ java/org/apache/jasper/el/ExpressionEvaluatorImpl.java
@@ -16,7 +16,6 @@
  */
 package org.apache.jasper.el;
 
-import javax.el.ELContext;
 import javax.el.ExpressionFactory;
 import javax.el.ValueExpression;
 import javax.servlet.jsp.el.ELException;
@@ -29,30 +28,31 @@ import javax.servlet.jsp.el.VariableResolver;
 
 public final class ExpressionEvaluatorImpl extends ExpressionEvaluator {
 
-	private final ExpressionFactory factory;
-	
-	public ExpressionEvaluatorImpl(ExpressionFactory factory) {
-		this.factory = factory;
-	}
+    private final ExpressionFactory factory;
+    
+    public ExpressionEvaluatorImpl(ExpressionFactory factory) {
+        this.factory = factory;
+    }
 
-	public Expression parseExpression(String expression, Class expectedType,
-			FunctionMapper fMapper) throws ELException {
-		try {
-			ELContextImpl ctx = new ELContextImpl(ELResolverImpl.DefaultResolver);
+    public Expression parseExpression(String expression, Class expectedType,
+            FunctionMapper fMapper) throws ELException {
+        try {
+            ELContextImpl ctx =
+                new ELContextImpl(ELResolverImpl.getDefaultResolver());
             if (fMapper != null) {
                 ctx.setFunctionMapper(new FunctionMapperImpl(fMapper));
             }
-			ValueExpression ve = this.factory.createValueExpression(ctx, expression, expectedType);
-			return new ExpressionImpl(ve);
-		} catch (javax.el.ELException e) {
-			throw new ELParseException(e.getMessage());
-		}
-	}
+            ValueExpression ve = this.factory.createValueExpression(ctx, expression, expectedType);
+            return new ExpressionImpl(ve);
+        } catch (javax.el.ELException e) {
+            throw new ELParseException(e.getMessage());
+        }
+    }
 
-	public Object evaluate(String expression, Class expectedType,
-			VariableResolver vResolver, FunctionMapper fMapper)
-			throws ELException {
-		return this.parseExpression(expression, expectedType, fMapper).evaluate(vResolver);
-	}
+    public Object evaluate(String expression, Class expectedType,
+            VariableResolver vResolver, FunctionMapper fMapper)
+            throws ELException {
+        return this.parseExpression(expression, expectedType, fMapper).evaluate(vResolver);
+    }
 
 }
