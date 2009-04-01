ec2273938df415f7a92751501721c0966fd6ad87
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2009-04-01 17:42:19 +0000

    - Move the fixmes and add getters.

--- trunk/java/org/apache/catalina/core/StandardWrapperValve.java
+++ trunk/java/org/apache/catalina/core/StandardWrapperValve.java
@@ -1,18 +1,23 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
 
@@ -399,6 +404,15 @@ final class StandardWrapperValve
         
         // FIXME: Implement async mode, which means invoking listeners or the Servlet
         // FIXME: Some async mode need creating a new filter chain
+        
+        /* FIXME:
+         * - If async context has path: remap and invoke Servlet
+         * - If async context has runnable, invoke runnable
+         * - If no path, reinvoke servlet with current filters
+         * - If no event mode (and end event) invoke listeners
+         * - If timeout, invoke listeners
+         * - If error ?
+         */
         
         // Get the current (unchanged) filter chain for this request
         ApplicationFilterChain filterChain = 
