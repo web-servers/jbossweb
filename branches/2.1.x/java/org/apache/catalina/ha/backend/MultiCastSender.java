a197db70b8f14b4f221124ea7fad4591ea68bd32
jfrederic.clere@jboss.com <jfrederic.clere@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2009-10-09 15:54:26 +0000

    Add a local bind for box with multi IP otherwise httpd may use the wrong port.

--- branches/2.1.x/java/org/apache/catalina/ha/backend/MultiCastSender.java
+++ branches/2.1.x/java/org/apache/catalina/ha/backend/MultiCastSender.java
@@ -22,6 +22,7 @@ import org.jboss.logging.Logger;
 
 import java.net.MulticastSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.DatagramPacket;
 import java.io.UnsupportedEncodingException;
 
@@ -47,7 +48,13 @@ public class MultiCastSender
         if (s == null) {
             try {
                 group = InetAddress.getByName(config.getGroup());
-                s = new MulticastSocket(config.getMultiport());
+                if (config.host != null) {
+                    InetAddress addr =  InetAddress.getByName(config.host);
+                    InetSocketAddress addrs = new InetSocketAddress(addr, config.getMultiport());
+                    s = new MulticastSocket(addrs);
+                } else
+                    s = new MulticastSocket(config.getMultiport());
+          
                 s.setTimeToLive(config.getTtl());
                 s.joinGroup(group);
             } catch (Exception ex) {
