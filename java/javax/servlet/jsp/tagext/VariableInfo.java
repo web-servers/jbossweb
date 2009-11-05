d90577f41e4cb0bf81f862ddad7d0ff8f44f13e3
remy.maucherat <rmaucher@users.noreply.github.com>
2009-11-05 16:28:02 +0000

    - Update JSP and EL API. - Skip resources from JBoss JARs.

--- java/javax/servlet/jsp/tagext/VariableInfo.java
+++ java/javax/servlet/jsp/tagext/VariableInfo.java
@@ -1,19 +1,56 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ *
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
  
 package javax.servlet.jsp.tagext;
@@ -61,7 +98,7 @@ package javax.servlet.jsp.tagext;
  * is known to the tag library and thus, delivered in the same JAR
  * file as the tag handlers. In most other remaining cases it will
  * refer to a class that is in the platform on which the JSP processor
- * is built (like J2EE). Using fully qualified class names in this
+ * is built (like Java EE). Using fully qualified class names in this
  * manner makes the usage relatively resistant to configuration
  * errors.
  *
@@ -106,7 +143,6 @@ package javax.servlet.jsp.tagext;
  * <blockquote>
  * <table cellpadding="2" cellspacing="2" border="0" width="55%"
  *        bgcolor="#999999" summary="Variable Synchronization Points">
- * <tbody>
  *   <tr align="center">
  *     <td valign="top" colspan="6" bgcolor="#999999"><u><b>Variable Synchronization
  *     Points</b></u><br>
@@ -176,7 +212,6 @@ package javax.servlet.jsp.tagext;
  *     <td valign="top" align="center" bgcolor="#ffffff">AT_BEGIN, AT_END<br>
  *     </td>
  *   </tr>
- * </tbody>
  * </table>
  * <sup>1</sup> Called after <code>doStartTag()</code> if 
  * <code>EVAL_BODY_INCLUDE</code> is returned, or after 
