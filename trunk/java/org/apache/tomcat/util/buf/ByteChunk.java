c8d65958a4d639b8899ecc27680c87f436e7d6f9
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2010-05-18 13:14:35 +0000

    - Port minor indexOf fix.

--- trunk/java/org/apache/tomcat/util/buf/ByteChunk.java
+++ trunk/java/org/apache/tomcat/util/buf/ByteChunk.java
@@ -691,22 +691,23 @@ public final class ByteChunk implements Cloneable, Serializable {
     }
 
     public int indexOf( String src, int srcOff, int srcLen, int myOff ) {
-	char first=src.charAt( srcOff );
+        char first=src.charAt( srcOff );
 
-	// Look for first char 
-	int srcEnd = srcOff + srcLen;
+        // Look for first char 
+        int srcEnd = srcOff + srcLen;
         
-	for( int i=myOff+start; i <= (end - srcLen); i++ ) {
-	    if( buff[i] != first ) continue;
-	    // found first char, now look for a match
+        mainLoop:
+        for( int i=myOff+start; i <= (end - srcLen); i++ ) {
+            if( buff[i] != first ) continue;
+            // found first char, now look for a match
             int myPos=i+1;
-	    for( int srcPos=srcOff + 1; srcPos< srcEnd; ) {
+            for( int srcPos=srcOff + 1; srcPos< srcEnd; ) {
                 if( buff[myPos++] != src.charAt( srcPos++ ))
-		    break;
-                if( srcPos==srcEnd ) return i-start; // found it
-	    }
-	}
-	return -1;
+                    continue mainLoop;
+            }
+            return i-start; // found it
+        }
+        return -1;
     }
 
     // -------------------- Hash code  --------------------
