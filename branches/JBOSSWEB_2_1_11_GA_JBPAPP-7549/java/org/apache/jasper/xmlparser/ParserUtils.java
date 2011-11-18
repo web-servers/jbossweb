0227b3fb9046fe0de2fb054ec0e9a8d676baa528
mmusaji <mmusaji@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2011-11-18 17:03:59 +0000

    JBPAPP-7549 - added fix to ParserUtils.java to wrap calls to no-arg calls with TCCL push-pop to jaspers defining class loader

--- branches/JBOSSWEB_2_1_11_GA_JBPAPP-7549/java/org/apache/jasper/xmlparser/ParserUtils.java
+++ branches/JBOSSWEB_2_1_11_GA_JBPAPP-7549/java/org/apache/jasper/xmlparser/ParserUtils.java
@@ -86,11 +86,11 @@ public class ParserUtils {
 
         // Perform an XML parse of this document, via JAXP
         try {
-            DocumentBuilderFactory factory =
-                DocumentBuilderFactory.newInstance();
-            factory.setNamespaceAware(true);
+        	DocumentBuilderFactory factory =
+        		createDocumentBuilderFactory();
+        		factory.setNamespaceAware(true);
             factory.setValidating(validating);
-            DocumentBuilder builder = factory.newDocumentBuilder();
+            DocumentBuilder builder = newDocumentBuilder(factory);
             builder.setEntityResolver(entityResolver);
             builder.setErrorHandler(errorHandler);
             document = builder.parse(is);
@@ -184,8 +184,37 @@ public class ParserUtils {
         // Return the completed TreeNode graph
         return (treeNode);
     }
-}
+    
+    protected static DocumentBuilderFactory createDocumentBuilderFactory()
+    {
+    	ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    	DocumentBuilderFactory factory;
+    	try
+    	{
+    		Thread.currentThread().setContextClassLoader(ParserUtils.class.getClassLoader());
+    		factory = DocumentBuilderFactory.newInstance();
+    	}
+    	finally
+    	{
+    		Thread.currentThread().setContextClassLoader(cl);
+    	}
+    	return factory;
+    }
 
+    protected static DocumentBuilder newDocumentBuilder(DocumentBuilderFactory factory) throws ParserConfigurationException
+    {
+    	ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    	try
+    	{
+    		Thread.currentThread().setContextClassLoader(ParserUtils.class.getClassLoader());
+    		return factory.newDocumentBuilder();
+    	}
+    	finally
+    	{
+    		Thread.currentThread().setContextClassLoader(cl);
+    	}
+    }
+}
 
 // ------------------------------------------------------------ Private Classes
 
