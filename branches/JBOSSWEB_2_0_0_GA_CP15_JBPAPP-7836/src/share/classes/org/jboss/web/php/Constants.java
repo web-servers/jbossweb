2bf638f950f0ab5343580e19609f70f03631c903
bmaxwell <bmaxwell@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2012-01-03 21:44:59 +0000

    [JBPAPP-7836] one off patch branch

---
+++ branches/JBOSSWEB_2_0_0_GA_CP15_JBPAPP-7836/src/share/classes/org/jboss/web/php/Constants.java
@@ -0,0 +1,37 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.web.php;
+
+
+/**
+ * Package Constants
+ *
+ * @author Mladen Turk
+ * @version $Revision: 4354 $, $Date: 2006-05-22 19:53:20 +0200 (lun., 22 mai 2006) $
+ * @since 1.0
+ */
+ public class Constants {
+
+    public static final String Package = "org.apache.catalina.servlets.php";
+
+}
