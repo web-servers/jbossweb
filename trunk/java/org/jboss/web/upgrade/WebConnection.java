ed5af0d453290f563cb2878a1711ae0bce64ac88
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2012-03-16 16:54:57 +0000

    Fix glitches and add javadoc.

--- trunk/java/org/jboss/web/upgrade/WebConnection.java
+++ trunk/java/org/jboss/web/upgrade/WebConnection.java
@@ -24,14 +24,89 @@ package org.jboss.web.upgrade;
 
 import java.io.IOException;
 
+/**
+ * The WebConnection object is provided by the web container and is used to allow
+ * access to the raw streams of the socket following an upgrade from HTTP/1.1.
+ * 
+ * @author remm
+ */
 public interface WebConnection {
+
+    /**
+     * Returns true when data may be read from the connection (the flag becomes false if no data
+     * is available to read). When the flag becomes false, the Servlet can attempt to read additional
+     * data, but it will block until data is available. If calling this method returns false, it will also 
+     * request notification when the connection has data available for reading again, and the  
+     * Servlet will be called back using the inputAvailable method of the ProtocolHandler.
+     * 
+     * @return boolean true if data can be read without blocking
+     */
     public boolean isReadReady();
+
+    /**
+     * Returns true when data may be written to the connection (the flag becomes false 
+     * when the client is unable to accept data fast enough). When the flag becomes false, 
+     * the Servlet must stop writing data. If there's an attempt to flush additional data 
+     * to the client and data still cannot be written immediately, an IOException will be 
+     * thrown. If calling this method returns false, it will also 
+     * request notification when the connection becomes available for writing again, and the  
+     * Servlet will be called back using the outputReady method of the ProtocolHandler.
+     * <br>
+     * Note: If the Servlet is not using isWriteReady, and is writing its output inside the
+     * container threads (inside the ProtocolHandler.resume() method processing, for example), 
+     * using this method is not mandatory, and writes will block until all bytes are written.
+     * 
+     * @return boolean true if data can be written without blocking
+     */
     public boolean isWriteReady();
+
+    /**
+     * This method sets the timeout in milliseconds of idle time on the connection.
+     * The timeout is reset every time data is received from the connection. If a timeout occurs, the 
+     * Servlet will be called back using the ProtocolHandler.timeout method which will not result in 
+     * automatically closing the connection (the connection may be closed using the close() method).
+     * 
+     * @param timeout The timeout in milliseconds for this connection, must be a positive value, larger than 0
+     */
     public void setTimeout(int timeout);
+
+    /**
+     * Suspend processing of the connection until the configured timeout occurs, 
+     * or resume() is called. In practice, this means the servlet will no longer 
+     * receive read method callbacks. Reading should always be performed synchronously in 
+     * the web container threads unless the connection has been suspended.
+     */
     public void suspend();
-    public void close() throws IOException;
+
+    /**
+     * Resume will cause the Servlet container to use the ProtocolHandler.resume()
+     * method to call back the Servlet, where the request can be processed synchronously 
+     * (for example, it is possible to use this to complete the request after 
+     * some asynchronous processing is done). This also resumes read events 
+     * if they have been disabled using suspend. It is then possible to call suspend 
+     * again later. It is also possible to call resume without calling suspend before.
+     * This method must be called asynchronously.
+     */
+    public void resume();
+
+    /**
+     * Close the connection. This will send back to the client a notice that the server 
+     * has no more data to send as part of this request. The ProtocolHandler.close method will 
+     * also be called by the container. This method will actually never perform any operation
+     * synchronously, it will merely mark the connection for closing.
+     */
+    public void close();
+
+    /**
+     * Read data in the given byte array.
+     */
     public int read(byte[] b, int off, int len)
             throws IOException;
+
+    /**
+     * Write the given bytes.
+     */
     public void write(byte[] b, int off, int len)
             throws IOException;
+
 }
