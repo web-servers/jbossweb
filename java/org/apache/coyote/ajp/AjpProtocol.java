aa089ca820d9d9ab7f4a439a309e14a571bc8661
Jean-Frederic Clere <jfclere@gmail.com>
2012-08-09 14:39:59 +0000

    Arrange r2033 = add the internal JMX names.

--- java/org/apache/coyote/ajp/AjpProtocol.java
+++ java/org/apache/coyote/ajp/AjpProtocol.java
@@ -186,14 +186,14 @@ public class AjpProtocol
             if (this.domain != null ) {
                 try {
                     tpOname = new ObjectName
-                    (domain + ":" + "type=ThreadPool,name=" + getName());
+                    (domain + ":" + "type=ThreadPool,name=" + getJmxName());
                     Registry.getRegistry(null, null)
                     .registerComponent(endpoint, tpOname, null );
                 } catch (Exception e) {
                     log.error("Can't register threadpool" );
                 }
                 rgOname = new ObjectName
-                (domain + ":type=GlobalRequestProcessor,name=" + getName());
+                (domain + ":type=GlobalRequestProcessor,name=" + getJmxName());
                 Registry.getRegistry(null, null).registerComponent
                 (cHandler.global, rgOname, null);
             }
