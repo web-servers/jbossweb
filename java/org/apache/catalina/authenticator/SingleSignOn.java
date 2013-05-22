52b2d4135e1929cc5f3593ff25019b61561c4e2f
remy.maucherat <rmaucher@users.noreply.github.com>
2013-05-22 15:48:23 +0000

    Modify SSO logout so that it can avoid expiring all sessions.

--- java/org/apache/catalina/authenticator/SingleSignOn.java
+++ java/org/apache/catalina/authenticator/SingleSignOn.java
@@ -458,6 +458,35 @@ public class SingleSignOn
 
     }
 
+
+    /**
+     * Logout the specified single sign on identifier from all sessions.
+     *
+     * @param ssoId Single sign on identifier to logout
+     */
+    public void logout(String ssoId) {
+
+        // Look up and remove the corresponding SingleSignOnEntry
+        SingleSignOnEntry sso = null;
+        synchronized (cache) {
+            sso = cache.get(ssoId);
+        }
+
+        if (sso == null)
+            return;
+
+        // Remove all authentication information from all associated sessions
+        Session sessions[] = sso.findSessions();
+        for (Session session : sessions) {
+            session.setAuthType(null);
+            session.setPrincipal(null);
+            session.removeNote(Constants.SESS_USERNAME_NOTE);
+            session.removeNote(Constants.SESS_PASSWORD_NOTE);
+        }
+
+    }
+
+
     /**
      * Deregister the specified session.  If it is the last session,
      * then also get rid of the single sign on identifier
