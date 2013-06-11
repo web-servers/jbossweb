c7d9d7036a3768de7901819c267d3a5b394111d1
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2013-06-11 11:18:58 +0000

    SSO logout should cleanup the entry credentials.

--- branches/8.0.x/src/main/java/org/apache/catalina/authenticator/SingleSignOn.java
+++ branches/8.0.x/src/main/java/org/apache/catalina/authenticator/SingleSignOn.java
@@ -539,6 +539,8 @@ public class SingleSignOn
             session.removeNote(Constants.SESS_USERNAME_NOTE);
             session.removeNote(Constants.SESS_PASSWORD_NOTE);
         }
+        // Reset SSO authentication
+        sso.updateCredentials(null, null, null, null);
 
     }
 
