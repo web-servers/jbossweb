58f5b67227531108a7afb2ec229c021d0785727d
tfonteyn <tfonteyn@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2013-08-14 13:14:09 +0000

    [WFLY-1589] allow configuration of requiredSecret attribute on the AJP connector with a system property

--- branches/8.0.x/src/main/java/org/apache/coyote/ajp/Constants.java
+++ branches/8.0.x/src/main/java/org/apache/coyote/ajp/Constants.java
@@ -46,7 +46,9 @@ public final class Constants {
     public static final boolean DEFAULT_TCP_NO_DELAY = true;
     public static final boolean DEFAULT_TOMCAT_AUTHENTICATION = 
             Boolean.valueOf(System.getProperty("org.apache.coyote.ajp.DEFAULT_TOMCAT_AUTHENTICATION", "true")).booleanValue();
-
+    public static final String DEFAULT_REQUIRED_SECRET = 
+            System.getProperty("org.apache.coyote.ajp.DEFAULT_REQUIRED_SECRET");
+    
     // Prefix codes for message types from server to container
     public static final byte JK_AJP13_FORWARD_REQUEST   = 2;
     public static final byte JK_AJP13_SHUTDOWN          = 7;
