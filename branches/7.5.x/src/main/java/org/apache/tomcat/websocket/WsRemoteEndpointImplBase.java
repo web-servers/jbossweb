b9bce1f737b083f7e2aac6a45f3cdedd6fee9276
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2014-11-20 16:14:55 +0000

    Port upstream websocket fixes.

--- branches/7.5.x/src/main/java/org/apache/tomcat/websocket/WsRemoteEndpointImplBase.java
+++ branches/7.5.x/src/main/java/org/apache/tomcat/websocket/WsRemoteEndpointImplBase.java
@@ -304,12 +304,10 @@ public abstract class WsRemoteEndpointImplBase implements RemoteEndpoint {
 
         boolean doWrite = false;
         synchronized (messagePartLock) {
-            if (Constants.OPCODE_CLOSE == mp.getOpCode()) {
-                try {
-                    setBatchingAllowed(false);
-                } catch (IOException e) {
-                    WebsocketsLogger.ROOT_LOGGER.flushOnCloseFailed(e);
-                }
+            if (Constants.OPCODE_CLOSE == mp.getOpCode() && getBatchingAllowed()) {
+                // Should not happen. To late to send batched messages now since
+                // the session has been closed. Complain loudly.
+                WebsocketsLogger.ROOT_LOGGER.cannotFlushOnClose();
             }
             if (messagePartInProgress) {
                 // When a control message is sent while another message is being
@@ -382,7 +380,10 @@ public abstract class WsRemoteEndpointImplBase implements RemoteEndpoint {
         if (Constants.INTERNAL_OPCODE_FLUSH == mp.getOpCode()) {
             nextFragmented = fragmented;
             nextText = text;
-            doWrite(mp.getEndHandler(), outputBuffer);
+            outputBuffer.flip();
+            SendHandler flushHandler = new OutputBufferFlushSendHandler(
+                    outputBuffer, mp.getEndHandler());
+            doWrite(flushHandler, outputBuffer);
             return;
         }
 
@@ -860,6 +861,30 @@ public abstract class WsRemoteEndpointImplBase implements RemoteEndpoint {
             }
         }
     }
+
+
+    /**
+     * Ensures that the output buffer is cleared after it has been flushed.
+     */
+    private static class OutputBufferFlushSendHandler implements SendHandler {
+
+        private final ByteBuffer outputBuffer;
+        private final SendHandler handler;
+
+        public OutputBufferFlushSendHandler(ByteBuffer outputBuffer, SendHandler handler) {
+            this.outputBuffer = outputBuffer;
+            this.handler = handler;
+        }
+
+        @Override
+        public void onResult(SendResult result) {
+            if (result.isOK()) {
+                outputBuffer.clear();
+            }
+            handler.onResult(result);
+        }
+    }
+
 
     private class WsOutputStream extends OutputStream {
 
