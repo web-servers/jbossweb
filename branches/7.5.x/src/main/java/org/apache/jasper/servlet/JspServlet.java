70899e5d31a68c278e61b8018ffa1da13b56a09a
remy.maucherat@jboss.com <remy.maucherat@jboss.com@1720a2a9-4018-0410-8ad1-ecba3f6e0018>
2017-04-27 14:52:16 +0000

    BZ1393221: Port Tomcat fix for CVE-2016-6796.

--- branches/7.5.x/src/main/java/org/apache/jasper/servlet/JspServlet.java
+++ branches/7.5.x/src/main/java/org/apache/jasper/servlet/JspServlet.java
@@ -78,6 +78,10 @@ public class JspServlet extends HttpServlet implements PeriodicEventListener {
             // Check for a custom Options implementation
             String engineOptionsName = 
                 config.getInitParameter("engineOptionsClass");
+            if (Constants.IS_SECURITY_ENABLED && engineOptionsName != null) {
+                JasperLogger.ROOT_LOGGER.settingIgnored("engineOptionsClass", engineOptionsName);
+                engineOptionsName = null;
+            }
             if (engineOptionsName != null) {
                 // Instantiate the indicated Options implementation
                 try {
